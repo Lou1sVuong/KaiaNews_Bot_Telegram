c9c4527a4c6fffe49ea10648da754e1f361c9950
Mai V. Xuan Vuong <xuannvuongg@gmail.com>
2024-06-11 18:28:34 +0700

    chore: Add new message for invalid command in USERS_MESSAGES constant

--- src/constants/message.ts
+++ src/constants/message.ts
@@ -1,4 +1,5 @@
 export const USERS_MESSAGES = {
   NO_CURRENCY: 'You have not chosen any currency yet',
-  NO_REGION: 'You have not chosen any region yet'
+  NO_REGION: 'You have not chosen any region yet',
+  Invalid_Command: 'Invalid command, please open menu and choose the command you want to use'
 }
