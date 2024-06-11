1252041fa0fd0fb2443ee289dcbb03f293d502e2
Mai V. Xuan Vuong <xuannvuongg@gmail.com>
2024-06-11 18:23:26 +0700

    chore: Remove commented out code for updating news

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { config } from 'dotenv'
 import { envConfig } from './constants/config'
 import { BotController } from './controllers/botController'
-import cron from 'node-cron'
 
 config()
 const token = envConfig.BOT_FATHER_TOKEN
@@ -14,8 +13,3 @@ const botController = new BotController(token)
 
 botController.init()
 console.log('Bot is running...')
-
-// // Thiết lập cron job để cập nhật tin tức mỗi giờ
-// cron.schedule('0 * * * *', () => {
-//   botController.updateNewsForAllUsers()
-// })
