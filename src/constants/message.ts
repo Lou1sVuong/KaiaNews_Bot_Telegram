c57d88b9f8d4575159e1648f2fa315de3af304b7
Mai V. Xuan Vuong <xuannvuongg@gmail.com>
2024-06-11 18:30:44 +0700

    Refactor invalid command message in USERS_MESSAGES constant

--- src/constants/message.ts
+++ src/constants/message.ts
@@ -1,5 +1,5 @@
 export const USERS_MESSAGES = {
   NO_CURRENCY: 'You have not chosen any currency yet',
   NO_REGION: 'You have not chosen any region yet',
-  Invalid_Command: 'Invalid command, please open menu and choose the command you want to use'
+  Invalid_Command: ' 🤖 Invalid command,💡 please open menu and choose the command you want to use '
 }
